a15327f47e8f2858398d60bff8575b99b0269cdf
Furydin <35685316+Furydins@users.noreply.github.com>
2026-09-22 12:17:32 +0100

    _ Resolve issue causing level time to become stuck

--- EasyExperienceBar.lua
+++ EasyExperienceBar.lua
@@ -433,6 +433,8 @@ function EasyExperienceBar.EventHandler(self, event, arg1, arg2, arg3, arg4, ...
         --print("TIME_PLAYED_MSG", arg1, arg2)
         EasyExperienceBar.session.realTotalTime = arg1 or 0
         EasyExperienceBar.session.realLevelTime = arg2 or 0
+        EasyExperienceBar.session.lastSessionLevelTime = arg2 or EasyExperienceBar.session.lastSessionLevelTime
+        EasyExperienceBar.session.lastSessionTotalTime = arg1 or EasyExperienceBar.session.lastSessionTotalTime 
         EasyExperienceBar:Update()
     elseif "PLAYER_XP_UPDATE" == event then
         local currentXP = _G.UnitXP("player") or 0
@@ -580,7 +582,6 @@ function EasyExperienceBar:RegisterEvents()
     EasyExperienceBar.MainFrame:RegisterEvent("PET_BATTLE_OPENING_START")
     EasyExperienceBar.MainFrame:RegisterEvent("PET_BATTLE_CLOSE")
 
-     EasyExperienceBar.UpdateTimer = _G.C_Timer.NewTicker(15, function() _G.RequestTimePlayed() end)
 end
 
 function EasyExperienceBar:CreateProgressBar(parent)
@@ -875,11 +876,11 @@ function EasyExperienceBar:CalculateValues()
         -- Direkte Werte aus TIME_PLAYED_MSG verwenden
         totalTime = EasyExperienceBar.session.realTotalTime or 0
         levelTime = EasyExperienceBar.session.realLevelTime or 0
-        --totalTime = (currentTime - EasyExperienceBar.currentTotalTimeStart) +
-        --              EasyExperienceBar.session.lastSessionTotalTime
-        --levelTime = (currentTime - EasyExperienceBar.currentSessionLevelStart) +
-        --           EasyExperienceBar.lastSessionLevelTime
-        --EasyExperienceBar.session.lastSessionLevelTime = levelTime
+        totalTime = (currentTime - EasyExperienceBar.currentTotalTimeStart) +
+                      EasyExperienceBar.session.lastSessionTotalTime
+        levelTime = (currentTime - EasyExperienceBar.currentSessionLevelStart) +
+                   EasyExperienceBar.lastSessionLevelTime
+        EasyExperienceBar.session.lastSessionLevelTime = levelTime
     end
 
     EasyExperienceBar.session.realLevelTime = levelTime
